6f032a5521d8d73de02533f513287934e714e403
Amar Hussain <99237742+amarhussain01@users.noreply.github.com>
2023-09-21 03:53:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// if statement = a basic of decision making
+//                if a condition is true, then do something
+//                if not, then don't do it!
+/*
+let age = 65;
+
+if(age >= 65){
+    console.log("you are a senior citizen!");
+}
+else if(age >= 18){
+    console.log("You are an adult");
+}
+else if(age < 0){
+    console.log("You Haven't Been Born Yet!");
+}
+
+else{
+    console.log("You are a child!");
+}
+*/
+
+let online = true;
+
+if(online){
+    console.log("you are online!");
+}
+
+else{
+    console.log("You are offline!");
+}
